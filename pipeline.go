ac7858b3dce7f08c8015656761d7430166b035a8
m-faried <mfaried@kemettrading.com>
2024-09-22 11:32:08 +0300

    feat: adding tokens count with example

--- pipeline.go
+++ pipeline.go
@@ -14,6 +14,7 @@ type IPipeline[I any] interface {
 	FeedMany(i []I)
 	ReadError() error
 	Append(p IPipeline[I])
+	TokensCount() uint64
 }
 
 type pipeline[I any] struct {
@@ -21,6 +22,9 @@ type pipeline[I any] struct {
 	resultStep  *ResultStep[I]
 	errorsQueue *Queue[error]
 	channelSize uint16
+
+	tokensCount uint64
+	tokensMutex sync.Mutex
 }
 
 func NewPipeline[I any](channelSize uint16, resultStep *ResultStep[I], steps ...*Step[I]) IPipeline[I] {
@@ -45,18 +49,16 @@ func (p *pipeline[I]) Init() {
 			allChannels[i] = make(chan I, p.channelSize)
 		}
 
-		// init steps
+		// setting channels for each step
 		for i := 0; i < stepsCount && (i+1) < channelsCount; i++ {
 			p.steps[i].input = allChannels[i]
 			p.steps[i].output = allChannels[i+1]
 			p.steps[i].errorsQueue = p.errorsQueue
 		}
 
-		// init result handler if exists
-		if p.resultStep == nil {
-			return
-		}
+		// setting the input for the result step.
 		p.resultStep.input = p.steps[stepsCount-1].output
+		p.resultStep.decrementTokensCount = p.decrementTokensCount
 	})
 }
 
@@ -65,12 +67,10 @@ func (p *pipeline[I]) Run(ctx context.Context) {
 	// creating a wait group for all the step routines.
 	wg := &sync.WaitGroup{}
 
-	// running the result step if it exists
-	if p.resultStep != nil {
-		for range p.resultStep.replicas {
-			wg.Add(1)
-			go p.resultStep.run(ctx, wg)
-		}
+	// running the result step.
+	for range p.resultStep.replicas {
+		wg.Add(1)
+		go p.resultStep.run(ctx, wg)
 	}
 
 	// running steps in reverse order
@@ -90,16 +90,18 @@ func (p *pipeline[I]) Run(ctx context.Context) {
 	for _, step := range p.steps {
 		close(step.input)
 	}
-	// closing the output of the last step which is the input to the result if it exists
+	// closing the output of the last step which is the input to the result
 	close(p.steps[len(p.steps)-1].output)
 }
 
 func (p *pipeline[I]) FeedOne(item I) {
+	p.incrementTokensCount()
 	p.steps[0].input <- item
 }
 
 func (p *pipeline[I]) FeedMany(items []I) {
 	for _, item := range items {
+		p.incrementTokensCount()
 		p.steps[0].input <- item
 	}
 }
@@ -113,4 +115,22 @@ func (p *pipeline[I]) Append(other IPipeline[I]) {
 	// input for the other is the output for the current.
 	pOther.steps[0].input = p.steps[len(p.steps)-1].output
 	p.steps = append(p.steps, pOther.steps...)
+}
+
+func (p *pipeline[I]) TokensCount() uint64 {
+	p.tokensMutex.Lock()
+	defer p.tokensMutex.Unlock()
+	return p.tokensCount
+}
+
+func (p *pipeline[I]) incrementTokensCount() {
+	p.tokensMutex.Lock()
+	defer p.tokensMutex.Unlock()
+	p.tokensCount++
+}
+
+func (p *pipeline[I]) decrementTokensCount() {
+	p.tokensMutex.Lock()
+	defer p.tokensMutex.Unlock()
+	p.tokensCount--
 }
