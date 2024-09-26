e8d1e5d6cf0d48e86ed27931ecd1ea28b17eb062
m-faried <m.a.faried@gmail.com>
2024-09-26 21:25:42 +0300

    feat: adding second draft of step aggregator which I am still not satisfied with

--- stepAggregator.go
+++ stepAggregator.go
@@ -3,49 +3,65 @@ package pipelines
 import (
 	"context"
 	"sync"
+	"time"
 )
 
 // StepAggregatorProcess is the definition of the function that will be executed by the step.
-type StepAggregatorProcess[I any] func(I) error
+// It returns the item (could be the pass through item or the result of aggregation)
+// a boolean whether this item is aggregated or not, and another boolean indicating the
+// type of item whether it is a pass through or an aggregation result.
+type StepAggregatorProcess[I any] func(I) (I, bool, bool)
 
 // StepAggregatorConfig is a struct that defines the configuration for an aggregator step
 type StepAggregatorConfig[I any] struct {
-	Label                 string
-	Replicas              uint16
-	ErrorHandler          ErrorHandler
-	AggregatedDataChannel <-chan I
-	Process               StepAggregatorProcess[I]
+	Label        string
+	Replicas     uint16
+	ErrorHandler ErrorHandler
+	Process      StepAggregatorProcess[I]
 }
 
 // stepAggregator is a struct that defines an aggregator step
 type stepAggregator[I any] struct {
 	stepBase[I]
-	aggregatedDataChannel <-chan I
-	process               StepAggregatorProcess[I]
+	process StepAggregatorProcess[I]
 }
 
 func (s *stepAggregator[I]) Run(ctx context.Context, wg *sync.WaitGroup) {
+	// The flag to skip decrementing the tokens count once to count
+	// for the aggregated data as a single item still in the pipelines.
+	// If we don't have this flag, the decrement may hit zero before
+	// the data in the aggregator is sent as aggregator result.
+	skipOnceForAggregatedToken := false
 	for {
 		select {
 		case <-ctx.Done():
 			wg.Done()
 			return
+		case <-time.NewTicker(1 * time.Second).C:
+
 		case i, ok := <-s.input:
 			if !ok {
 				return
 			}
-			err := s.process(i)
-			if err != nil && s.errorHandler != nil {
-				s.errorHandler(s.label, err)
+			item, aggregated, aggregationResult := s.process(i)
+			if aggregated {
+				// we need to skip decrementing once since the aggregated element is going to
+				if skipOnceForAggregatedToken {
+					s.decrementTokensCount()
+				} else {
+					skipOnceForAggregatedToken = true
+				}
+			} else if aggregationResult {
+				// resetting the skip once flag.
+				skipOnceForAggregatedToken = false
+				// passing the aggregation result to the next step in the pipeline
+				s.output <- item
+			} else {
+				// since that the element was not aggregated
+				// we can pass it through to the next step in
+				// the pipeline
+				s.output <- item
 			}
-			// whether there is an error or something is done, we have to decrement the tokens count.
-			s.decrementTokensCount()
-		case aggData, ok := <-s.aggregatedDataChannel:
-			if !ok {
-				return
-			}
-			s.incrementTokensCount()
-			s.output <- aggData
 		}
 	}
 }
