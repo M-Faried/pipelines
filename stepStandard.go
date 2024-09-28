f2cbbeb42a018b9cda5befc4e83c975ead1ca795
m-faried <m.a.faried@gmail.com>
2024-09-28 18:25:29 +0300

    refactor: adding comments to step standard

--- stepStandard.go
+++ stepStandard.go
@@ -10,10 +10,17 @@ type StepProcess[I any] func(I) (I, error)
 
 // StepConfig is a struct that defines the configuration for a standard step
 type StepConfig[I any] struct {
-	Label        string
-	Replicas     uint16
+	// Label is a human-readable label for the step
+	Label string
+
+	// Replicas is the number of replicas of the step that should be run in parallel
+	Replicas uint16
+
+	// ErrorHandler is a function that will be called when an error occurs in the step
 	ErrorHandler ErrorHandler
-	Process      StepProcess[I]
+
+	// Process is a function that will be applied to the incoming data
+	Process StepProcess[I]
 }
 
 type stepStandard[I any] struct {
