de84207c32f78f8f21f2512587ad2132a1a6f3d4
m-faried <m.a.faried@gmail.com>
2024-10-04 01:59:05 +0300

    feat: adding the leaky bucket example

---
+++ examples/exampleLeakyBucket.go
@@ -0,0 +1,120 @@
+package examples
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	pip "github.com/m-faried/pipelines"
+)
+
+const TEXT = "piplines is an awesome package !!!"
+
+type leakyBucket struct {
+	addedTokensCount int
+	mutex            sync.Mutex
+}
+
+func (l *leakyBucket) inputTriggered(buffer []string) (string, pip.BufferFlags) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.addedTokensCount < len(buffer) {
+		l.addedTokensCount++
+	}
+	return "", pip.BufferFlags{}
+}
+
+func (l *leakyBucket) timeTriggeredProcess(buffer []string) (string, pip.BufferFlags) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	itemIndex := len(buffer) - l.addedTokensCount
+
+	if l.addedTokensCount > 1 {
+		l.addedTokensCount--
+	} else if l.addedTokensCount == 1 {
+		l.addedTokensCount = len(buffer)
+	}
+
+	return buffer[itemIndex], pip.BufferFlags{SendProcessOuput: true}
+}
+
+func createLeakyBucketPipeline() pip.IPipeline[string] {
+	builder := &pip.Builder[string]{}
+
+	wordSplitter := builder.NewStep(pip.StepFragmenterConfig[string]{
+		Label: "word splitter",
+		Process: func(in string) []string {
+			return strings.Split(in, " ")
+		},
+	})
+
+	wordTrimmer := builder.NewStep(pip.StepBasicConfig[string]{
+		Label: "word trimmer",
+		Process: func(in string) string {
+			return strings.Trim(in, " ,;:")
+		},
+	})
+
+	wordFilter := builder.NewStep(pip.StepFilterConfig[string]{
+		Label: "filter",
+		PassCriteria: func(in string) bool {
+			return in != ""
+		},
+	})
+
+	leakyBucket := &leakyBucket{}
+	bucketStep := builder.NewStep(pip.StepBufferConfig[string]{
+		Label:                        "Leaky Bucket Step",
+		BufferSize:                   10,
+		InputTriggeredProcess:        leakyBucket.inputTriggered,
+		TimeTriggeredProcess:         leakyBucket.timeTriggeredProcess,
+		TimeTriggeredProcessInterval: 1 * time.Second,
+	})
+
+	printer := builder.NewStep(pip.StepTerminalConfig[string]{
+		Label: "terminal",
+		Process: func(in string) {
+			fmt.Println(in)
+		},
+	})
+
+	pipeline := builder.NewPipeline(1, wordSplitter, wordTrimmer, wordFilter, bucketStep, printer)
+	pipeline.Init()
+	return pipeline
+}
+
+func ExampleLeakyBucket() {
+
+	// Set up a channel to listen for interrupt signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	pipeline := createLeakyBucketPipeline()
+	pipeline.Run(ctx)
+	pipeline.FeedOne(TEXT)
+
+	// Goroutine to cancel the context when an interrupt signal is received
+	go func() {
+		<-sigChan
+		fmt.Println("Interrupt signal received, cancelling context...")
+
+		// cancel the context
+		cancelCtx()
+
+		// terminating the pipeline and clearning resources
+		pipeline.Terminate()
+
+		fmt.Println("Pipeline Terminated!!!")
+	}()
+
+	<-ctx.Done()
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("Example Leaky Bucket Done!!!")
+}
---
+++ examples/exampleLeakyBucket.md
@@ -0,0 +1,17 @@
+# Leaky Bucket Example Description
+
+This is an example how to use the buffer step to implement the leaky bucket algorithm.
+
+The bucket should receive a fixed size string and does the following:
+
+1. Split the string into words.
+
+2. Removes the puncetuation marks.
+
+3. Display the words 1 by 1 with the 1 second interval.
+
+4. When it reaches the end, it should start from the beginning.
+
+### Tip
+
+Try remove the buffer step and watch what happens.
